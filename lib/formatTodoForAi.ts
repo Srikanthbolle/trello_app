816a0de8665b38e5468df673326e4779e617bc4c
Srikanthbolle <bollesrikanth186@gmail.com>
2025-05-14 14:36:13 +0530

    update modal issues

--- lib/formatTodoForAi.ts
+++ lib/formatTodoForAi.ts
@@ -2,18 +2,19 @@ import { Board, Todo, TypedColumn } from "../typings"
 
 const formatTodoForAi=(board:Board)=>{
     const todos=Array.from(board.columns.entries());
-    const flatArray = todos.reduce((map,[key,value])=>{
-        map[key] =value.todos;
-        return map
-    },{} as { [key in TypedColumn]:Todo[]});
-    //reduce to key:value(length)
-    const flattedArrayCounted = Object.entries((flatArray).reduce(
-        (map,[key,value])=>{
-            map[key as TypedColumn] = value.length;
-            return map;
+    const flatArray = todos.reduce((map, [key, value]) => {
+        map[key] = value.todos;
+        return map;
+      }, {} as { [key in TypedColumn]: Todo[] });
+      
+      const flattedArrayCounted = Object.entries(flatArray).reduce(
+        (map, [key, value]) => {
+          map[key as TypedColumn] = value.length;
+          return map;
         },
-        {} as { [key in TypedColumn]:number}
-    ))
+        {} as { [key in TypedColumn]: number }
+      );
+      
 return flattedArrayCounted;
 }
 export default formatTodoForAi;
